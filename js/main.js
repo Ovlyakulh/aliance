9d5ad87489c12442c82440c6d3271607c719211c
Ovlyakulh <ovlyakulh@gmail.com>
2026-08-08 06:56:41 +0200

    add: mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".burger-link");
+const menu = document.querySelector(".mobile-menu");
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
@@ -9,3 +11,7 @@ window.addEventListener('scroll', () => {
     logo.href.baseVal = "images/sprit.svg#logo-light";
   }
 });
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open")
+})
