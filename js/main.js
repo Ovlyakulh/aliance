08266f556881d30dfe108e1331814f7080fd9d95
Ovlyakul <aos899293@gmail.com>
2026-08-09 09:58:26 +0200

    fix: mobile-menu main.js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,36 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".burger-link");
 const menu = document.querySelector(".mobile-menu");
-window.addEventListener('scroll', () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "images/sprit.svg#logo";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "images/sprit.svg#logo-light";
-  }
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "images/sprit.svg#logo";
+};
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "images/sprit.svg#logo-light";
+};
+
+const openMenu = (event) => {
+  menu.classList.add("is-open"); //Вешает класс is-open
+  mMenuToggle.classList.add("close-menu")
+  document.body.style.overflow = "hidden"; // Запрешает прокр  утку сайта под меню
+  lightModeOn();
+};
+
+const closeMenu = (event) => {
+  menu.classList.remove("is-open"); //Удаляет класс is-open
+  mMenuToggle.classList.remove("close-menu")
+  document.body.style.overflow = ""; // Возврашает прокрутку
+  //  сайта под меню
+  lightModeOff();
+};
+
+window.addEventListener("scroll", () => {
+  this.scrollY > 1 ? lightModeOn() : lightModeOff()
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open")
-})
+ menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
